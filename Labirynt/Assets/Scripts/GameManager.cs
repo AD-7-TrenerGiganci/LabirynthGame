8c0b04f9a9b8bc0ecfc2a944b22e2cba92934d47
AD-7-TrenerGiganci <olek100115@o2.pl>
2021-02-22 20:02:23 +0100

    lekcja 2/3

---
+++ Labirynt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int timeToEnd;
+
+
+    bool ifPaused = false;
+    bool ifEndGame = false;
+    bool ifWin = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        InvokeRepeating("Timer", 2, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (ifPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void Timer()
+    {
+        timeToEnd--;
+
+        if(timeToEnd <= 0)
+        {
+            ifEndGame = true;
+            timeToEnd = 0;
+        }
+        if (ifEndGame)
+        {
+            EndGame();
+        }
+
+        Debug.Log("Time: " + timeToEnd + " s");
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Pause Game");
+        ifPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume Game");
+        ifPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void EndGame()
+    {
+        CancelInvoke("Timer");
+        Time.timeScale = 0f;
+        if (ifWin)
+        {
+            Debug.Log("YOU WON");
+        }
+        else
+        {
+            Debug.Log("YOU LOSE");
+        }
+
+    }
+
+}
---
+++ Labirynt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float sprintModifier = 1.5f;
+    public float gravity = -9.81f;
+
+    CharacterController characterController;
+    Vector3 velocity = new Vector3();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = x * transform.right + z * transform.forward;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            characterController.Move(direction * speed * sprintModifier * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(direction * speed * Time.deltaTime);
+        }
+       
+
+      
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+       
+    }
+}
